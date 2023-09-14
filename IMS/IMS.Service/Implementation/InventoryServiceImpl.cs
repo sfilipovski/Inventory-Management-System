ade5c57450f4ede0f97d8cfc684f31fa4a013ec6
sfilipovski <49661929+sfilipovski@users.noreply.github.com>
2023-09-15 00:37:43 +0200

    Initial project

---
+++ IMS/IMS.Service/Implementation/InventoryServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using IMS.Domain.DomainModels;
+using IMS.Repository.Implementation;
+using IMS.Repository.Interface;
+using IMS.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Service.Implementation
+{
+    public class InventoryServiceImpl : InventoryService
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public InventoryServiceImpl(IInventoryRepository _inventoryRepository)
+        {
+            this._inventoryRepository = _inventoryRepository;
+        }
+    }
+}
